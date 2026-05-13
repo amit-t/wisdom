5beedfc54c00dba68fee3123649b53dccd01ad46
amit-t <tiwari.m.amit@gmail.com>
2026-05-13 08:09:21 +0530

    p1: stub modules for P3 subcommands

---
+++ lib/cmd_import_url.zsh
@@ -0,0 +1,4 @@
+_wisdom_import_url() {
+  print -r -- "wisdom import-url: not implemented yet (Phase 3)" >&2
+  return 0
+}
---
+++ lib/cmd_show.zsh
@@ -0,0 +1,4 @@
+_wisdom_show() {
+  print -r -- "wisdom show: not implemented yet (Phase 3)" >&2
+  return 0
+}
